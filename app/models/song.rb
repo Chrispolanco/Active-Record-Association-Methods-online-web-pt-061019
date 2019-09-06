915ab94a8e491d32708ace1fe113a095b77e71d7
Christian Polanco <chrispolanco@sbcglobal.net>
2019-09-06 05:31:19 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,6 +7,8 @@ class Song < ActiveRecord::Base
   end
 
   def drake_made_this
-    drake = Artist.find_or_create_by(name: 'Drake')
+    drake = Artist.find_or_create_by(name: "Drake")
     self.artist = drake
+  end 
+  
 end
